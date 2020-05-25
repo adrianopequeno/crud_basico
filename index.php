bb606f2820ad4677f85d7cfa085bfbc4180ce07c
Adriano Pequeno <37118906+adrianopequeno@users.noreply.github.com>
2020-05-25 16:40:48 -0300

    Atualizando redirecionamento no Index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       <td><?=$usuario['nome'];?></td>
       <td><?=$usuario['email'];?></td>
       <td>
-        <a href="editar.php/?id=<?=$usuario['id'];?>">[ Editar ]</a>
+        <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
         <a href="excluir.php?id=<?=$usuario['id'];?>" onclick="return confirm('Tem certeza que deseja excluir?')">[ Excluir ]</a>
       </td>
     </tr>
